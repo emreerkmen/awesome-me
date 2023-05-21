14ee50d7eb4b2f8ff645b85e9601009b5aad3786
Emre Erkmen <emre.erkmen@hotmail.com>
2023-05-21 23:02:55 +0300

    Fix margin of divs

--- app/page.tsx
+++ app/page.tsx
@@ -20,17 +20,17 @@ export default function Home() {
           className="profil-pic">
         </Image>
       </div>
-      <div className="center">
+      <div className="flex-container center">
         <h1 className='' >Here is my resume</h1>
         <h2>Summary</h2>
-        <p>As a 5 years experienced senior software engineer, my expertise in designing microservice-based REST APIs using Java with Spring MVC and Spring Boot frameworks, Golang with Gorilla Mux library, and tools like Postman has resulted in the deployment of efficient and fault-tolerant systems. I have a proven track record of developing reusable, high-quality, and secure code while implementing unit tests. I have successfully managed projects utilizing Agile Scrum methodology, leading to improved efficiency and estimation accuracy. My experience with technologies such as React JS, Docker, and Openshift has allowed me to build scalable and performant web applications while maintaining JBoss and Apache web servers on Linux. Additionally, I have mentored developers, reviewed code, and collaborated with teams to develop new features and functionality while using Confluence and SourceTree for collaboration.</p>
+        <p className="box-shadow">As a 5 years experienced senior software engineer, my expertise in designing microservice-based REST APIs using Java with Spring MVC and Spring Boot frameworks, Golang with Gorilla Mux library, and tools like Postman has resulted in the deployment of efficient and fault-tolerant systems. I have a proven track record of developing reusable, high-quality, and secure code while implementing unit tests. I have successfully managed projects utilizing Agile Scrum methodology, leading to improved efficiency and estimation accuracy. My experience with technologies such as React JS, Docker, and Openshift has allowed me to build scalable and performant web applications while maintaining JBoss and Apache web servers on Linux. Additionally, I have mentored developers, reviewed code, and collaborated with teams to develop new features and functionality while using Confluence and SourceTree for collaboration.</p>
         <h2>GarantiBBVA Technology</h2>
-        <p>Expert Software Engineer - May, 2022 - Present</p>
+        <p >Expert Software Engineer - May, 2022 - Present</p>
 
-        <p>Responsible for project of development and maintaining garantibbva.com.tr, the
+        <p className="box-shadow">Responsible for project of development and maintaining garantibbva.com.tr, the
           website of Turkey&apos;s second largest private bank GarantiBBVA and CMS used
           throughout the company. My Responsibilities;</p>
-        <ul className='bullet'>
+        <ul className='bullet box-shadow'>
           <li>Proficient in designing micro service based REST APIs using Java with Spring MVC, Spring Boot frameworks and Golang with Gorilla Mux library and utilizing tools like Postman for API testing and documentation.</li>
           <li>Developed reusable, high-quality, testable, and secure (using SonarLint) code. Documented the code and implemented unit tests.</li>
           <li>Created and managed containerized applications using Docker and Openshift.</li>
