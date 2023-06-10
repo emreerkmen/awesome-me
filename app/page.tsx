9c06d5cb744e13715a9bdbd4503fecf808cadb18
Emre Erkmen <emre.erkmen@hotmail.com>
2023-06-10 20:27:14 +0300

    Add cool profil pic

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import BlogText from "../components/blog/BlogText";
 import { ExperienceData, getSortedExperiencesData } from "../utils/experiences";
 
 export default async function Home() {
-
   const experiences: ExperienceData | ExperienceData[] =
     await getSortedExperiencesData().then((data) => data);
 
@@ -15,23 +14,54 @@ export default async function Home() {
           <code className="">Space</code>
         </p>
       </div>
-
-      <div className="">
-        <Image
-          src="./profile/picture.jpg"
-          width={80}
-          height={100}
-          alt="Picture of the author"
-          loading="lazy"
-          className="profil-pic"
-        ></Image>
+      <div className="container">
+        <div className="name">
+          <h1>
+            Hi! I am <br></br>
+            <span className="highlight">Emre Erkmen</span>
+          </h1>
+          <p>
+            I am a Software Engineer I am a Software Engineer I am a Software
+            Engineer I am a Software Engineer I am a Software Engineer
+          </p>
+        </div>
+        <div className="image">
+          <Image
+            src="./profile/blue_blob.svg"
+            width={80}
+            height={100}
+            alt="blob"
+            loading="lazy"
+            className="blob-one"
+          ></Image>
+          <Image
+            src="./profile/orange_blob.svg"
+            width={80}
+            height={100}
+            alt="blob second"
+            loading="lazy"
+            className="blob-second"
+          ></Image>
+          <Image
+            src="./profile/pic.svg"
+            width={80}
+            height={100}
+            alt="Picture of the author"
+            loading="lazy"
+            className="profil-pic"
+          ></Image>
+        </div>
       </div>
       <div className="flex-container center">
         <h1 className="">Here is my resume</h1>
         <h2>Summary</h2>
         {experiences.map(({ id, content, ...data }: ExperienceData) => (
           <div key={id}>
-            <BlogText date={data.date} title={data.title} contentHtml={content}></BlogText>
+            <BlogText
+              date={data.date}
+              title={data.title}
+              contentHtml={content}
+            ></BlogText>
           </div>
         ))}
       </div>
